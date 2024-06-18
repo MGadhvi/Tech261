46ae379823d4e2d6836e08be1ad2fcbf13035a46
Manish Gadhvi <mgadhvi@spartaglobal.com>
2024-06-18 15:41:54 +0100

    Added new project for Abstract Classes and Interfaces

---
+++ AbstractClassesAndInterfaces/src/main/java/com/sparta/mg/Dog.java
@@ -0,0 +1,44 @@
+package com.sparta.mg;
+
+public class Dog {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        //int age = 5
+        Dog max = new Dog("Max", "Terrier", 2);
+        Dog fido = new Dog("Fido", "Terrier", 10);
+        System.out.println(max.getName());
+    }
+}
