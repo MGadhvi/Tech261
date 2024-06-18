12ba9d93c7954551167570ed4a6d27d78b9a98ef
Manish Gadhvi <mgadhvi@spartaglobal.com>
2024-06-18 16:09:54 +0100

    ÂAdded Abstract class examples with Cat, Dog and Animal

---
+++ AbstractClassesAndInterfaces/src/main/java/com/sparta/mg/App.java
@@ -0,0 +1,11 @@
+package com.sparta.mg;
+
+public class App {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Max", "Terrier", 10);
+        Cat cat = new Cat("Bob", "Cat", 12);
+
+        cat.play();
+        dog.play();
+    }
+}
