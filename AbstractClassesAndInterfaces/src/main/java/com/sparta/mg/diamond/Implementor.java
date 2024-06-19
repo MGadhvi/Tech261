d328792ab107a840c0441dce90afd0956ff7038e
Manish Gadhvi <mgadhvi@spartaglobal.com>
2024-06-19 14:40:44 +0100

    Added session for Static and Final

--- AbstractClassesAndInterfaces/src/main/java/com/sparta/mg/diamond/Implementor.java
+++ AbstractClassesAndInterfaces/src/main/java/com/sparta/mg/diamond/Implementor.java
@@ -1,8 +1,9 @@
 package com.sparta.mg.diamond;
 
-public class Implementor implements InterfaceOne, InterfaceTwo{
+public class Implementor implements InterfaceOne, InterfaceTwo {
+
     @Override
     public String diamondMethod() {
-        return InterfaceTwo.super.diamondMethod();
+        return InterfaceOne.super.diamondMethod();
     }
 }
