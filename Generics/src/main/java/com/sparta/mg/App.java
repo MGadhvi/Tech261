95381765b4b1b333aa3c9f225836444b18d3e0d4
Manish Gadhvi <mgadhvi@spartaglobal.com>
2024-06-26 13:34:20 +0100

    Added session on Design Patterns

--- Generics/src/main/java/com/sparta/mg/App.java
+++ Generics/src/main/java/com/sparta/mg/App.java
@@ -15,11 +15,11 @@ public class App {
         System.out.printf("Object Integer Rectangle Area: %d\n", (Integer)objectRectangle1.getLength() * (Integer) objectRectangle1.getWidth());
         System.out.printf("Object Double Rectangle Area: %f\n", (Double)objectRectangle2.getLength() * (Double) objectRectangle2.getWidth());
 
-        GenericRectangle<? extends > integerGenericRectangle = new GenericRectangle<>(2, 4);
+        GenericRectangle<? extends Number> integerGenericRectangle = new GenericRectangle<>(2, 4);
 
         GenericRectangle<Double> doubleRectangleGenericRectangle = new GenericRectangle<>(2.0, 4.0);
 
-        System.out.printf("Generic Integer Rectangle Area: %d\n", integerGenericRectangle.getHeight() * integerGenericRectangle.getWidth());
+        System.out.printf("Generic Integer Rectangle Area: %d\n", (Integer)integerGenericRectangle.getHeight() * (Integer)integerGenericRectangle.getWidth()); //Casting still needed
         System.out.printf("Generic Double Rectangle Area: %f\n", doubleRectangleGenericRectangle.getHeight() * doubleRectangleGenericRectangle.getWidth());
 
     }
