bddbf94249d86394b7b1ca964d3611c41913fdd2
Manish Gadhvi <mgadhvi@spartaglobal.com>
2024-06-26 11:40:10 +0100

    ammended code for generics session

---
+++ Generics/src/main/java/com/sparta/mg/App.java
@@ -0,0 +1,26 @@
+package com.sparta.mg;
+
+import java.util.TreeSet;
+
+public class App {
+    public static void main(String[] args) {
+        IntegerRectangle integerRectangle = new IntegerRectangle(2, 4);
+        DoubleRectangle doubleRectangle = new DoubleRectangle(2.0, 4.0);
+        System.out.printf("Integer Rectangle Area: %d\n", integerRectangle.getWidth() * integerRectangle.getHeight());
+        System.out.printf("Double Rectangle Area: %f\n", doubleRectangle.getWidth() * doubleRectangle.getLength());
+
+        ObjectRectangle objectRectangle1 = new ObjectRectangle(2, 4);
+        ObjectRectangle objectRectangle2 = new ObjectRectangle(2.0, 4.0);
+
+        System.out.printf("Object Integer Rectangle Area: %d\n", (Integer)objectRectangle1.getLength() * (Integer) objectRectangle1.getWidth());
+        System.out.printf("Object Double Rectangle Area: %f\n", (Double)objectRectangle2.getLength() * (Double) objectRectangle2.getWidth());
+
+        GenericRectangle<? extends > integerGenericRectangle = new GenericRectangle<>(2, 4);
+
+        GenericRectangle<Double> doubleRectangleGenericRectangle = new GenericRectangle<>(2.0, 4.0);
+
+        System.out.printf("Generic Integer Rectangle Area: %d\n", integerGenericRectangle.getHeight() * integerGenericRectangle.getWidth());
+        System.out.printf("Generic Double Rectangle Area: %f\n", doubleRectangleGenericRectangle.getHeight() * doubleRectangleGenericRectangle.getWidth());
+
+    }
+}
---
+++ Generics/src/main/java/com/sparta/mg/GenericRectangle.java
@@ -0,0 +1,37 @@
+package com.sparta.mg;
+
+//T - Type
+//V - Value
+//K,V - Key, Value
+//E - Element
+
+public class GenericRectangle<T> {
+
+    private T width;
+    private T height;
+
+    public GenericRectangle(T width, T height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public T getWidth() {
+        return width;
+    }
+
+    public void setWidth(T width) {
+        this.width = width;
+    }
+
+    public T getHeight() {
+        return height;
+    }
+
+    public void setHeight(T height) {
+        this.height = height;
+    }
+
+    public static <V extends Integer> void printToConsole(V value) {
+        System.out.println("Rectangle Area: " + value);
+    }
+}
